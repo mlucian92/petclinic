f105da6d248da19d492c22f89a67cf9e85bfaaff
mlucian92 <lucian.mircioaga@cst.ro>
2020-04-01 17:52:43 +0300

    Adding Index Page and controller

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,12 +1,12 @@
 package guru.springframework.sfgpetclinic.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
-@Builder
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PetType extends BaseEntity{
